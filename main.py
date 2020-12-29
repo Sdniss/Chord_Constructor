967cb13f56602d53463a5c21b9d8d9edabe3fff2
Stijn Denissen <sdenissen@Thijss-MacBook-Air.local>
2020-12-29 02:20:37 +0100

    [1] Add pdf report, [2] Add figures

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import pandas as pd
-from tools import ChordGenerator
+from tools import ChordGenerator, PDF
 
 # Initiate object and call desired methods
-chord_generator = ChordGenerator('C', 'dorian')
+chord_generator = ChordGenerator('C', 'lydian')
+
+# Get the chords according with the scale
 chord_generator.get_chords(chord_length='3-4-5-6-7')
+
+# Get midi samples
 chord_generator.generate_midi_samples()
 
-# Create dataframe
+# Create dataframe for the above methods
 chords_df = pd.DataFrame(chord_generator.chords_dict)\
     .T\
     .sort_index()  # Transpose so that chordnames are indices and sort on them
-chords_df.columns = range(1,8)  # Rename so that columns are 1-7 (1st note corresponds with column 1)
+chords_df.columns = [f'Note {x}' for x in range(1,8)]  # Rename
+chords_df.insert(0, 'Chord', chords_df.index)  # Necessary for printing the pdf
 
+# Create figure
+chord_generator.get_figures()
+
+# Create report
+# PDF creation section
+pdf = PDF('Stijn Denissen', f'{chord_generator.key}_{chord_generator.mode}')
+pdf.write_header('Mode Notes', 1)
+pdf.add_figure(f'{chord_generator.figures_path}/{chord_generator.key}_{chord_generator.mode}.png')
+pdf.write_text(f'{chord_generator.key}_{chord_generator.mode}: {str(chord_generator.mode_scale)}')
+pdf.write_header('Chord Table', 1)
+pdf.add_table(chords_df)
+pdf.save(path = 'output/')
--- tools.py
+++ tools.py
@@ -1,10 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import os
+import re
 from midiutil import MIDIFile
+from datetime import date
+from fpdf import FPDF
 
-# Use this reference for interpretation of modes etc: https://bandnotes.info/tidbits/tidbits-feb.htm
 
-
+# Use this reference for interpretation of modes etc:
 class ChordGenerator:
     def __init__(self, key, church_mode):
         """ Set the key and mode
@@ -14,7 +17,8 @@ class ChordGenerator:
         """
         self.key = key
         self.mode = church_mode
-        self.resource = 'https://feelyoursound.com/scale-chords/'
+        self.resource_1 = 'https://feelyoursound.com/scale-chords/'
+        self.resource_2 = 'https://bandnotes.info/tidbits/tidbits-feb.htm'
 
         # 2 types of chromatic scales, one with sharps and one with flats
         self.chromatic_sharps = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
@@ -59,7 +63,7 @@ class ChordGenerator:
                                  'Minor 7th': [0, -1, 0, -1],
                                  'Major 7th': [0,0,0,0],
                                  'Diminished 7th': [0,-1,-1,-2],
-                                 'Half Diminished 7th': [0,-1,-1,-1],
+                                 'Half Dim 7th': [0,-1,-1,-1],
                                  '6th': [0,0,0,0],
                                  'Minor 6th': [0,-1,0,0],
                                  'Added 9th': [0,0,0,0],
@@ -79,6 +83,8 @@ class ChordGenerator:
 
         # Initiations of variables that are generated with the functions
         self.chords_dict = None
+        self.midi_path = None
+        self.figures_path = None
 
     def get_chords(self, chord_length = '3-4'):
         """ Get all chords with prespecified lengths
@@ -142,7 +148,8 @@ class ChordGenerator:
 
         # Create folder in the output folder
         new_folder_path = f'{os.getcwd()}/output/midi/{self.key}_{self.mode}'
-        print(new_folder_path)
+        self.midi_path = new_folder_path
+
         if not os.path.exists(new_folder_path):
             os.makedirs(new_folder_path)
 
@@ -182,8 +189,76 @@ class ChordGenerator:
             with open(f"{new_folder_path}/{chord_name}.mid", "wb") as output_file:  # Write the midi file
                 MyMIDI.writeFile(output_file)
 
-    def get_info(self):
-        pass
+    def get_figures(self):
+
+        # Create folder in the output folder
+        new_folder_path = f'{os.getcwd()}/output/figures/{self.key}_{self.mode}'
+        self.figures_path = new_folder_path
+        if not os.path.exists(new_folder_path):
+            os.makedirs(new_folder_path)
+
+        # Get chromatic and roll it back to C in front
+        notes_note_bar = ['C','D','E','F','G','A','B']
+
+        # Get position on the note bar
+        first_note = notes_note_bar.index(self.key)
+        note_positions = range(first_note, first_note + 7)
+
+        # Get list of sharps and flats. Nothing if
+        sharps_flats_list = [re.findall('[#|b]', note)[0] if re.findall('[#|b]', note) else '' for note in self.mode_scale]
+
+        # Plot mode
+        fig, ax = plt.subplots()
+        for x_pos, y_pos, sharp_flat in zip(range(1,1 + len(note_positions)), note_positions, sharps_flats_list):
+            ax.scatter(x_pos, y_pos, color = 'k', s= 500)
+            ax.text(x_pos+0.2, y_pos+0.2, sharp_flat, fontdict = {'size': 20})
+
+        ax.axhline(y=0, xmin = 0, xmax = 1/10, color = 'k')
+        for y_pos in [2, 4, 6, 8, 10]:  # Bar lines
+            ax.axhline(y = y_pos, color = 'k')
+
+        ax.set_title(f'{self.key} {self.mode}')
+        plt.axis('off')
+        fig.savefig(f'{new_folder_path}/{self.key}_{self.mode}.png')
+        plt.close()
+
+        # Plot all chords
+        for chord_name, chord in self.chords_dict.items():
+            chord = [note for note in chord if note != '']  # Remove empty instances
+
+            # Get chromatic and roll it back to C in front
+            notes_note_bar = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'A', 'B']
+
+            # Get first position on the note bar
+            first_note_position = notes_note_bar.index(re.findall('[A-Z]', chord[0])[0])
+
+            # Get note positions.
+            note_positions = []
+            for note in chord:
+                indices = np.argwhere(notes_note_bar == note)  # Use instead of .index() since it only takes first match
+                for index in indices:
+                    if index > first_note_position:
+                        note_positions.append(index)
+
+            note_positions = [notes_note_bar.index(re.findall('[A-Z]', note)[0]) for note in chord]
+
+            # Get list of sharps and flats. Nothing if
+            sharps_flats_list = [re.findall('[#|b]', note)[0] if re.findall('[#|b]', note) else '' for note in
+                                 chord]
+
+            fig, ax = plt.subplots()
+            for y_pos, sharp_flat in zip(note_positions, sharps_flats_list):
+                ax.scatter(0, y_pos, color='k', s=500)
+                ax.text(0, y_pos, sharp_flat, fontdict={'size': 20, 'color': 'red'})
+
+            ax.axhline(y=0, xmin=0.4, xmax=0.6, color='k')
+            for y_pos in [2, 4, 6, 8, 10]:  # Bar lines
+                ax.axhline(y=y_pos, color='k')
+
+            ax.set_title(chord_name)
+            plt.axis('off')
+            fig.savefig(f'{new_folder_path}/{chord_name}.png')
+            plt.close()
 
 
 # Internal functions
@@ -364,3 +439,191 @@ def _mode_notes_from_chromatic(big_dict, mode_scale):
                 sub_dict.update({Note: value})
 
     return sub_dict
+
+
+# Resource: https://towardsdatascience.com/creating-pdf-files-with-python-ad3ccadfae0f
+class PDF(FPDF):
+
+    # Initiate the pdf, add a title page
+    def __init__(self, author, project):
+        super().__init__()  # Because we defined class PDF upon class FPDF
+        self.pdf_h = 297  # height of A4 in mm
+        self.pdf_w = 210  # width of A4 in mm
+        self.author = author
+        self.project = project
+        self.date = date.today()
+        self.add_title_page()
+        self.fontsize_dict = {'regular': 10,
+                              'header_1': 30,
+                              'header_2': 20,
+                              'header_3': 15}
+        self.line_height_dict = {'regular': 5,
+                                 'header_1': 15,
+                                 'header_2': 10,
+                                 'header_3': 7.5}
+
+        # Preferences for all normal pages after having added a title page
+        self.set_font('Arial', style = '', size = 10)
+        self.set_text_color(r=60,g=60,b=60)
+        self.set_auto_page_break(auto=True, margin=35)
+        self.set_top_margin(35)  # No bottom margin necessary since set_auto_page_break takes care of this
+        self.set_left_margin(20)
+        self.set_right_margin(20)
+        self.add_page()  # Create a second page where subsequent operations will start from
+
+    def add_title_page(self):
+        # General
+        pdf_w = self.pdf_w
+        pdf_h = self.pdf_h
+        self.add_page()
+        self.set_text_color(128,128,128)
+
+        # Big Title
+        self.set_y(pdf_h/2)  # CAVE: Order important. set_y always before set_x. set_y resets set_x (1048 in fpdf.py)
+        self.set_x(pdf_w/6)
+        self.set_font('Arial', 'B', 40)
+        title = self.project
+        max_title_len = 15
+        if len(title) >= max_title_len:
+            raise ValueError(f'Please use a project title with less than {max_title_len} characters')
+        self.cell(w=pdf_w, txt = title, border = 0, ln=0, align='L')
+
+        # Author info
+        self.set_y(pdf_h/2 + 12)
+        self.set_x(pdf_w/6)
+        self.set_font('Arial', 'I', 15)
+        self.cell(w=pdf_w, txt = f'By: {self.author}', border = 0, ln=0, align='L')
+
+    def add_horizontal_line(self):
+        # Settings
+        # line_height = self.fontsize_dict.get('regular') * self.point_size
+        self.set_line_width(0.0)
+        self.set_draw_color(220,220,220)
+        margin_left_right = 20
+
+        # Write empty line since "self.line" doesn't update y position
+        self.write(self.line_height_dict.get('regular'), '\n')
+
+        # Draw the line
+        y = self.get_y()
+        self.line(margin_left_right, y, self.pdf_w-margin_left_right, y)
+
+        # Write an additional empty line so the next writing starts at the right position
+        self.write(self.line_height_dict.get('regular'), '\n')
+
+    def write_header(self, text, header_type):
+        """ Print header text
+        :param text: the text you want to print
+        :param header_type: choose from [1, 2, 3]
+        """
+        # Start with some whitespace for each header
+        self.set_y(self.get_y() + 4)
+
+        # Write the header
+        self.set_font('Arial', 'BUI', self.fontsize_dict.get(f'header_{header_type}'))
+        self.write(self.line_height_dict.get(f'header_{header_type}'), f'{text}\n')  # Line break at the end
+
+        # Restore default
+        self.set_font('Arial', '', self.fontsize_dict.get('regular'))
+
+    def write_text(self, text):
+        """ This makes sure to always write (self.write) with the same line height
+        Mitigates the need to define it every time you want to write something
+        :param text: str, the text you want to write
+        """
+        self.write(self.line_height_dict.get('regular'), f'{text}\n')  # add line break after each text section
+
+    def add_table(self, df):
+
+        # Start with whitespace
+        self.write(self.line_height_dict.get('regular'), '\n')  # Start again from the left
+
+        # Settings for the table
+        table_font = 6
+        self.set_font('Courier', style = '', size = table_font)  # Use Courier for tables for fixed width
+
+        # Additional calculations
+        reference_page = self.page_no()
+        n_rows = df.shape[0]
+        n_cols = df.shape[1]
+        cell_width = 20
+        cell_height = 4
+        max_width = self.pdf_w - self.l_margin - self.r_margin
+        max_cells = int(np.floor(max_width/cell_width))
+
+        # Check if printing is at all possible
+        if df.shape[1] > max_cells:
+            raise ValueError(f'Please enter a maximum of {max_cells} columns')
+
+        # Print the columns
+        self.set_font('Courier', 'BI', size=table_font)
+        for column in df.columns:
+            self.cell(cell_width, cell_height, column[:10], 1, 0, 'C')  # Only get first 10 chars of columns
+        self.set_font('Courier', '', size=table_font)
+
+        self.write(cell_height, '\n')  # Start again from the left
+
+        # Loop over all cells in the dataframe
+        column_count = 0
+        for i in range(n_rows):
+
+            # Print columns again if cells come on a new page, and update reference page
+            if reference_page != self.page_no():
+                self.set_font('Courier','BI',size=table_font)
+                for column in df.columns:
+                    self.cell(cell_width, cell_height, column[:10], 1, 0, 'C')  # Only get first 10 chars of columns
+                self.write(cell_height, '\n')  # Start again from the left
+                self.set_font('Courier','',size=table_font)
+                reference_page += 1
+
+            # Print a row
+            for j in range(n_cols):
+                column_count += 1
+                value = df.iloc[i, j]
+                self.cell(cell_width, cell_height, str(value), 1, 0, 'C')
+
+            self.write(cell_height, '\n')  # Start again from the left
+
+        # Restore default and add whitespace
+        self.set_font('Arial', '', self.fontsize_dict.get('regular'))
+        self.write(self.line_height_dict.get('regular'), '\n')
+
+    def add_figure(self, path_or_url, image_h = None):
+        if image_h is None:
+            image_h = 30
+        space_left_on_page = self.pdf_h-self.b_margin - self.get_y()
+        if image_h > space_left_on_page:
+            self.add_page()
+        self.image(path_or_url, x = self.l_margin, y = self.get_y(), h = image_h)
+        self.set_y(self.get_y()+image_h)
+
+    def save(self, path = '', name = None):
+        """ By default saves the image as the name of your project in the same folder
+        :param path: str, path to the location where you want to store the image. Default is empty, so same folder
+        :param name: str, optional. Default is the project name where spaces replaced by underscores
+        """
+        if name is None:
+            name = '_'.join(self.project.split(' '))
+        self.output(name=f'{path}{name}.pdf', dest='F')
+
+    def footer(self):
+        """
+        Purposefully this overwrites the built-in "footer" function of fpdf
+        Now, every page that you add with "add_page()" will have this footer
+        """
+        self.set_y(-15)
+        self.set_text_color(128,128,128)
+        self.set_font('Arial', 'I', 9)
+        self.cell(0, 10, f'{self.project} - {self.page_no()}', 0, 0, 'C')
+        self.image('pdf_layout_images/lower_right.png', x=140, y=263, w=70)
+        self.set_x(10)
+        self.cell(0, 10, f'{self.date.day}/{self.date.month}/{self.date.year}', 0, 0, 'L')
+
+    def header(self):
+        """
+        Purposefully this overwrites the built-in "header" function of fpdf
+        Now, every page that you add with "add_page()" will have this header
+        """
+        pdf_w = self.pdf_w
+        self.image('pdf_layout_images/aims_logo.jpg', x = 2*(pdf_w/3), y = 0, w=pdf_w/3)
+        self.image('pdf_layout_images/upper_left.png', x = 0, y = 0, w=70)
